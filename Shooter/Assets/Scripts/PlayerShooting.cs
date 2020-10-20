57e9bd7191410f14e046e7884e096bde779668d7
piotrwojtaszek <piotrwojtaszek@users.noreply.github.com>
2020-10-20 17:19:17 +0200

    Prototyp

---
+++ Shooter/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public LineRenderer m_line;
+    public Transform m_firePoint;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 100f))
+        {
+            if (hit.transform.gameObject.GetComponent<BaseStats>())
+            {
+                hit.transform.gameObject.GetComponent<BaseStats>().TakeDamage(20f);
+            }
+            StartCoroutine(Line(hit, true));
+        }
+        else
+        {
+            StartCoroutine(Line(hit, false));
+        }
+
+    }
+
+    IEnumerator Line(RaycastHit hitInfo, bool hitted)
+    {
+
+        if (hitted)
+        {
+            m_line.SetPosition(0, m_firePoint.position);
+            m_line.SetPosition(1, hitInfo.point);
+        }
+        else
+        {
+            m_line.SetPosition(0, m_firePoint.position);
+            m_line.SetPosition(1, Camera.main.transform.forward * 100f);
+        }
+        m_line.enabled = true;
+        yield return new WaitForSeconds(0.1f);
+        m_line.enabled = false;
+    }
+}
