e6b89c5e6b9c42189bc704db1518e032e601d7e0
Bartosz Urban <b.urban@neurosys.com>
2020-11-01 19:15:03 +0100

    Enemy template classes

---
+++ Shooter/Assets/Scripts/Enemy/Enemy_Basic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Enemy_Basic : Enemy, BaseStats
+{
+    [SerializeField]
+    public int Health { get; set; }
+
+    public override void Init()
+    {
+
+    }
+
+    public void Damage()
+    {
+
+
+    }
+
+    public override void Movement()
+    {
+
+    }
+
+}
