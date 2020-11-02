8d982a36919e0cf43c90fc967a29346daa92292a
piotrwojtaszek <piotrwojtaszek@users.noreply.github.com>
2020-11-02 08:51:25 +0100

    Interface

---
+++ Shooter/Assets/Scripts/Interface/IBaseStats.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IBaseStats
+{
+    int Health { get; set; }
+    void TakeDamage(float amount);
+    void Die();
+}
