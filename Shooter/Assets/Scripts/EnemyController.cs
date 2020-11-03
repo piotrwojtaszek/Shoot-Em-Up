1a24a99bfa0e5c07fb2645c2f1417c55294e907c
wisniewskipiotr <piotr.t.wisniewski@gmail.com>
2020-11-03 14:07:07 +0100

    NavMeshAgent

--- Shooter/Assets/Scripts/EnemyController.cs
+++ Shooter/Assets/Scripts/EnemyController.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
 
     public float moveSpeed;
     public Rigidbody theRB;
+    private bool chasing;
+    private float distanceToChase = 10f, distanceToLose = 15f;
+    private Vector3 target;
 
+    public NavMeshAgent agent;
     
 
     // Start is called before the first frame update
@@ -19,8 +24,33 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(PlayerMovementController.instance.transform.position);
+        target = PlayerMovementController.instance.transform.position;
 
-        theRB.velocity = transform.forward * moveSpeed;
+        if (!chasing)
+        {
+            if (Vector3.Distance(transform.position, PlayerMovementController.instance.transform.position) < distanceToChase)
+            {
+                chasing = true;
+            }
+
+
+        }
+        else
+        {
+
+
+            //transform.LookAt(PlayerMovementController.instance.transform.position);
+
+            //theRB.velocity = transform.forward * moveSpeed;
+
+            agent.destination = target;
+
+
+            if (Vector3.Distance(transform.position, PlayerMovementController.instance.transform.position) > distanceToLose)
+            {
+                chasing = false;
+            }
+
+        }
     }
 }
