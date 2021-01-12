982042a0993ab7a0b3848e6828ffa7a157ca31d5
piotrwojtaszek <piotrwojtaszek@users.noreply.github.com>
2021-01-12 12:44:06 +0100

    Menu

---
+++ Shooter/Assets/Scripts/Menu/LghtColorChanger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LghtColorChanger : MonoBehaviour
+{
+    Light swiatelko;
+    [SerializeField]
+    float hue=0;
+    Color rgbColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        swiatelko = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hue += Time.deltaTime/12f;
+        hue %= 1;
+        rgbColor = Color.HSVToRGB(hue,1,1);
+        
+        swiatelko.color = rgbColor;
+    }
+}
