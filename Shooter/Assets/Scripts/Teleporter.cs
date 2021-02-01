31cd13be8118ef176f30e8d30eecb27b53a3d4ae
Piotr Wojtaszek <48589767+piotrwojtaszek@users.noreply.github.com>
2021-02-01 20:12:28 +0100

    Teleport And Lightmap

--- Shooter/Assets/Scripts/Teleporter.cs
+++ Shooter/Assets/Scripts/Teleporter.cs
@@ -4,25 +4,12 @@ using UnityEngine;
 
 public class Teleporter : MonoBehaviour
 {
+    [SerializeField]
+    Transform destination;
+ 
 
-    public float positionX = -10.0f;
-    public float positionY = 7.25f;
-    public float positionZ = -10.0f;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     void OnTriggerEnter(Collider col)
     {
-        col.transform.position = new Vector3(positionX, positionY, positionZ);
+        col.transform.position = destination.position;
     }
 }
