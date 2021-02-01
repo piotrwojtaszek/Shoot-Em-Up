30815e82dd46a3a9cba4e2118c95b643aa593136
Piotr Wojtaszek <48589767+piotrwojtaszek@users.noreply.github.com>
2021-02-02 00:31:43 +0100

    FinishLane

--- Shooter/Assets/Scripts/Player/FinishLane.cs
+++ Shooter/Assets/Scripts/Player/FinishLane.cs
@@ -1,20 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class FinishLane : MonoBehaviour
 {
     [SerializeField]
     GameObject canvas;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float levelTime = 0f;
+    [SerializeField]
+    TextMeshProUGUI czas;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        levelTime += Time.deltaTime;
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        canvas.SetActive(true);
+        czas.text = levelTime.ToString("F2");
+        Time.timeScale = 0f;
+
+    }
+
 }
